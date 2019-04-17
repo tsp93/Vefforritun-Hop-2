6b66a1fce38d3939312dc3f922369a104f0dfea9
T-Beard <thomasspalsson@gmail.com>
2019-04-17 17:32:47 +0000

    CSS fyrir categories komið

--- src/routes/categories/Categories.tsx
+++ src/routes/categories/Categories.tsx
@@ -1,44 +1,47 @@
 import React, { useState, useEffect, Fragment } from 'react';
-import  { Redirect,NavLink } from 'react-router-dom';
+import  { Link } from 'react-router-dom';
 
 import { getAllCategories } from '../../api';
 import { ICategory } from '../../api/types';
-import Category from '../category/Category';
 
+import './Categories.scss';
+
+/**
+ * Birtir vöruflokkana á forsíðunni
+ */
 export default function CategoriesRoute() {
 
   const [ categories , setCategories ] = useState();
   useEffect(() => {
-    const fetchProduct = async () =>{
+    const fetchProduct = async () => {
       const result = await getAllCategories();
       setCategories(result);
-    }
+    };
     fetchProduct();
   },[]);
   
-  function showCategories(cat: ICategory[]){
-    if(cat !== undefined){
-      let array : any = [];
-      for(let i=0; i<cat.length;i++){
+  function showCategories(cat: ICategory[]) {
+    if (cat !== undefined) {
+      const array : any = [];
+      for (let i = 0; i < cat.length; i += 1) {
         array.push(
-          <NavLink to={"/categories/"+cat[i].id } key={i}>
-          <div>
-            <h2>{cat[i].title}</h2>
-          </div>
-          </NavLink>
+          <Link to = {`/categories/${cat[i].id}`} key={i} className='category'>
+            <h1>{cat[i].title}</h1>
+          </Link>
         );
       }
-      return array;   
+      return array;
      }
   }
 
-
   return (
     <Fragment>
-      <section>
-        { showCategories(categories) }
-      </section>
-      
+      <div>
+        <h2>Skoðaðu vöruflokkana okkar</h2>
+        <section className='categories'>
+          { showCategories(categories) }
+        </section>
+      </div>
     </Fragment>
   );
 }
