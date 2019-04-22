edfd5d02249222a666d8eafef29cb40136aa7fad
T-Beard <thomasspalsson@gmail.com>
2019-04-22 15:58:27 +0000

    NotFound síða komin

--- src/routes/system-pages/NotFound.tsx
+++ src/routes/system-pages/NotFound.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './SystemPages.scss';
-import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
     <div className="system-page">
-      NotFound
+      <h1 className="errorTitle">404</h1>
+      <p className="errorMessage">Síða fannst ekki</p>
+      <Link className="errorLink" to='/'>Aftur á forsíðu</Link>
     </div>
   )
 }
